762b1b530e83572ceade0e678736eda2c150bf3b
Emmanuel Arhu <emmanuelarhu706@gmail.com>
2024-10-05 15:13:49 +0000

    New test

---
+++ tests/hubtelblog.spec.js
@@ -0,0 +1,107 @@
+// @ts-check
+// const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
+
+
+test('has title', async ({ page }) => {
+  await page.goto('https://blog.hubtel.com/category/news/');
+
+  
+  console.log(await page.title());
+  
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+test('has title1', async ({ page }) => {
+  await page.goto('https://blog.hubtel.com/category/news/');
+    
+    // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+  
+});
+
+
+// test('has title', async ({ page }) => {
+//     await page.goto('https://blog.hubtel.com/category/news/');
+    
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+  
+// });
+
+
+
+// test('has title', async ({ page }) => {
+//     await page.goto('https://blog.hubtel.com/category/news/');
+    
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+  
+// });
+
+
+
+
+
+
+// test('has title', async ({ page }) => {
+//     await page.goto('https://blog.hubtel.com/category/news/');
+    
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+  
+// });
+
+
+
+
+
+
+// test('has title', async ({ page }) => {
+//     await page.goto('https://blog.hubtel.com/category/news/');
+    
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+  
+// });
+
+
+
+
+
+
+// test('has title', async ({ page }) => {
+//     await page.goto('https://blog.hubtel.com/category/news/');
+    
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+  
+// });
+
+
+
+
+
+
+
+
+
+
+// test('has title', async ({ page }) => {
+//     await page.goto('https://blog.hubtel.com/category/news/');
+    
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+  
+// });
