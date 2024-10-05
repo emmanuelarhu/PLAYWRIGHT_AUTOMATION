6bff0b0d0ebd8e07f3521c23efedc3ed73c3e1ec
Emmanuel Arhu <emmanuelarhu706@gmail.com>
2024-10-05 22:27:39 +0000

    new test

--- tests/hubtelblog-footer.spec.js
+++ tests/hubtelblog-footer.spec.js
@@ -2,13 +2,37 @@
 // const { test, expect } = require('@playwright/test');
 import { test, expect } from '@playwright/test';
 
+test.describe('testing for hubtel blog site', () => {
+  test.only('has title', async ({page}) => {
+    await page.goto('https://blog.hubtel.com/category/news/');
+    await expect(page).toHaveTitle('News Archives - Hubtel Blog')
 
-test('has title', async ({ page }) => {
-  await page.goto('https://blog.hubtel.com/category/news/');
+  })
+})
 
-  
-  console.log(await page.title());
-  
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle('News Archives - Hubtel Blog');
-});
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let lager = 0; lager < 10; lager++) {
+
+//     test('has title' + lager, async ({ page }) => {
+//         await page.goto('https://blog.hubtel.com/category/news/');
+      
+        
+//         console.log(await page.title());
+        
+//         // Expect a title "to contain" a substring.
+//         await expect(page).toHaveTitle('News Archives - Hubtel Blog');
+//       });
+      
+// }
+
